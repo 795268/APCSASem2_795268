4a2cc1474287e3cd35371d56b76c1a3646a9f90b
795268 <42563195+795268@users.noreply.github.com>
2019-01-10 10:22:02 -0800

    finsihed sorting methods lab 109

--- labs/lab109/sort.java
+++ labs/lab109/sort.java
@@ -7,24 +7,69 @@
  */
 public class sort
 {
-    public static void sort(){
-        int small = 0; 
-        int nums[] = new int[]{3, 6, 2, 5, 7, 1, 0, 8, 5, 1};
-        int order[] = new int [nums.length];
-        int i  = 0;
-        while(i< nums.length){
-            for (int x = 0; x<nums.length; x++){
+    public static void mySort(){
+        int small = 0; //int to find the smallest unsorted values
+        int nums[] = new int[]{3, 6, 2, 5, 7, 1, 0, 8, 5, 1}; //unsorted array
+        int order[] = new int [nums.length]; // new  sorted array
+        int i  = 0; 
+        while(i< nums.length){ //keeps track of length of sorted array
+            for (int x = 0; x<nums.length; x++){ //traverses nums
 
-                if (nums[x] == small){
-                    order[i] =small;
-                    i++;
+                if (nums[x] == small){ // if the value = small
+                    order[i] =small; //assign that value to order
+                    i++; //increment to keep track of length of sorted array
                 }
 
             }
-            small ++; 
+            small ++; // increment small - looking for next biggest int
         }
-        for (int j = 0; j< order.length; j++){
-        System.out.println(order[j]); 
+        for (int j = 0; j< order.length; j++){ // loop to print all values of sorted array
+            System.out.println(order[j]); 
+        }
     }
-}
+
+    public static void bubbleSort(){
+        int nums[] = new int[]{3, 6, 2, 5, 7, 1, 0, 8, 5, 1}; //unsorted array
+        int temp;
+        for (int j=nums.length-1; j >0; j--){
+            for (int i = 0; i< j; i++){
+                if (nums[i] > nums[i+1]) { 
+                    temp = nums [i];
+                    nums [i] = nums [i+1]; 
+                    nums [i+1] = temp; 
+                }
+            }
+            for (int x = 0; x< nums.length; x++){ // loop to print all values of sorted array
+                System.out.print(nums[x]); 
+            }
+             System.out.print("\n");
+        }
+        // for (int x = 0; x< nums.length; x++){ // loop to print all values of sorted array
+            // System.out.print(nums[x]); 
+        // }
+        //System.out.print("\n");
+    }
+    
+    public static void selectSort(){
+        int small = 0; //int to find the smallest unsorted values
+        int nums[] = new int[]{3, 6, 2, 5, 7, 1, 0, 8, 5, 1}; //unsorted array
+        int order[] = new int [nums.length]; // new  sorted array
+        int i  = 0; 
+        while(i< nums.length){ //keeps track of length of sorted array
+            for (int x = 0; x<nums.length; x++){ //traverses nums
+
+                if (nums[x] == small){ // if the value = small
+                    order[i] =small; //assign that value to order
+                    i++; //increment to keep track of length of sorted array
+                }
+
+            }
+            small ++; // increment small - looking for next biggest int
+        }
+        for (int j = 0; j< order.length; j++){ // loop to print all values of sorted array
+            System.out.println(order[j]); 
+        }
+    }
+
+
 }
