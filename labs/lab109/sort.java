578239fe12a5d9a2e641426f8e2fcb56e2b12761
795268 <42563195+795268@users.noreply.github.com>
2019-01-09 22:42:42 -0800

    tried lab 109

---
+++ labs/lab109/sort.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class lan109 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class sort
+{
+    public static void sort(){
+        int small = 0; 
+        int nums[] = new int[]{3, 6, 2, 5, 7, 1, 0, 8, 5, 1};
+        int order[] = new int [nums.length];
+
+        int i  = 0;
+        for (int x = 0; x<nums.length; x++){
+
+            if (nums[x] == small){
+                order[nums[x]] =small;
+                i++;
+            }
+            if (nums[x] != small) {
+                i++;
+            }
+                    System.out.println(order[x]); 
+        }
+
+    }
+}
