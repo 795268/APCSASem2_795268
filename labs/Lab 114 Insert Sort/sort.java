4d6d87f4baba22a5fff33994e7e9dd83c97978d7
795268 <42563195+795268@users.noreply.github.com>
2019-01-14 10:18:31 -0800

    finished lab 1/14 a

--- labs/Lab 114 Insert Sort/sort.java
+++ labs/Lab 114 Insert Sort/sort.java
@@ -1,9 +1,9 @@
 
 /**
- * Write a description of class lan109 here.
+ * Write a description of class sort here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @Elena Campell
+ * @Adding insert sort and metrics 
  */
 public class sort
 {
@@ -31,45 +31,80 @@ public class sort
     public static void bubbleSort(){
         int nums[] = new int[]{3, 6, 2, 5, 7, 1, 0, 8, 5, 1}; //unsorted array
         int temp;
+        int swap = 0; 
+        int compare =0; 
         for (int j=nums.length-1; j >0; j--){
             for (int i = 0; i< j; i++){
                 if (nums[i] > nums[i+1]) { 
                     temp = nums [i];
                     nums [i] = nums [i+1]; 
                     nums [i+1] = temp; 
+                    swap ++; 
                 }
+                compare++;
+
             }
-            for (int x = 0; x< nums.length; x++){ // loop to print all values of sorted array
-                System.out.print(nums[x]); 
-            }
-            System.out.print("\n");
+
         }
-        // for (int x = 0; x< nums.length; x++){ // loop to print all values of sorted array
-        // System.out.print(nums[x]); 
-        // }
-        //System.out.print("\n");
+        for (int x = 0; x< nums.length; x++){ // loop to print all values of sorted array
+            System.out.print(nums[x]); 
+        }
+        System.out.print("\n");
+        System.out.println("swap:" + swap); 
+        System.out.print("compare:" + compare); 
     }
 
     public static void selectSort(){
 
         int nums[] = new int[]{3, 6, 2, 5, 7, 1, 0, 8, 5, 1}; //unsorted array
         int temp; 
+        int swap = 0; 
+        int compare =0; 
         for (int i  = 0; i < nums.length -1; i++){ //keeps track of length of sorted array
             int small = i; //int to find the smallest unsorted values
             for (int j = i+1; j <nums.length; j++){ //traverses nums
 
                 if (nums[j] < nums[small]){ // if the value = small
                     small =j; 
+                    
                 }
+                compare++;
 
             }
             temp = nums [small];
             nums [small] = nums [i]; 
             nums [i] = temp; 
+            swap++;
         }
         for (int k = 0; k< nums.length; k++){ // loop to print all values of sorted array
             System.out.println(nums[k]); 
         }
+        System.out.println("swap:" + swap); 
+        System.out.print("compare:" + compare); 
+    }
+
+    public static void insertSort() {
+        int nums[] = new int[]{3, 6, 2, 5, 7, 1, 0, 8, 5, 1}; //unsorted array
+        int temp;
+        int swap = 0; 
+        int compare =0; 
+        for (int i = 1; i < nums.length; i++){
+            for (int j = i; j > 0; j--){
+                if (nums[j] < nums[j-1]) { // if the current value is less, swap
+                    temp = nums [j];
+                    nums [j] = nums [j-1]; 
+                    nums [j-1] = temp; 
+                    swap++;
+                }
+                compare++;
+            }
+        }
+        for (int k = 0; k< nums.length; k++){ // loop to print all values of sorted array
+            System.out.println(nums[k]); 
+        }
+        System.out.println("swap:" + swap); 
+        System.out.print("compare:" + compare); 
     }
 
 }
+
