4c1563037dfca9f8cdca17ea9ad79417bf8f93f6
795268 <42563195+795268@users.noreply.github.com>
2019-01-31 10:33:56 -0800

    started lab 131 graphing

---
+++ labs/lab 131 graphing application/MySwingApplication.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class MySwingApplication here.
+ *
+Elena campell
+ * @lab 131
+ */
+import javax.swing.*;
+public class MySwingApplication implements Runnable
+{
+    public void run(){
+    }
+    public void main(){
+    }
+}
