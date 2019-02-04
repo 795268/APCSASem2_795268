e46fe5a7c7949a824a329f4985c7a791f3e029e7
795268 <42563195+795268@users.noreply.github.com>
2019-02-04 09:40:12 -0800

    finsihed activity1

--- labs/lab 131 graphing application/MySwingApplication.java
+++ labs/lab 131 graphing application/MySwingApplication.java
@@ -8,8 +8,15 @@ Elena campell
 import javax.swing.*;
 public class MySwingApplication implements Runnable
 {
+    JFrame jFrame; 
     public void run(){
+        JFrame jFrame = new JFrame("Window"); 
+        jFrame.setSize(500,679);
+        jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
     }
+
     public void main(){
     }
+
 }
