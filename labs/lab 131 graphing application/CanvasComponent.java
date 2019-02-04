fdb81d0e2b2315597674f439513607c6961e83f3
795268 <42563195+795268@users.noreply.github.com>
2019-02-04 10:29:26 -0800

    activity 2

--- labs/lab 131 graphing application/CanvasComponent.java
+++ labs/lab 131 graphing application/CanvasComponent.java
@@ -2,12 +2,30 @@
 /**
  * Write a description of class CanvasComponent here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ *Elena Campell
+ *activity 2
  */
 import java.awt.*; 
 import javax.swing.*;
 public class CanvasComponent extends JComponent
 {
+    CanvasComponent rect = new CanvasComponent(0, 0); 
+    int height; 
+    int width; 
+    int x; 
+    int y; 
+    public CanvasComponent(int width, int height){
+        rect.setSize(width, height); 
+        height = 40;
+        width = 30;
+        x =40;
+        y= 20; 
+        
+    }
+    
+    protected void paintComponent(Graphics g){
+        g.setColor(Color.red); 
+        g.fillRect(40,30,60,70); 
+    }
     
 }
--- labs/lab 131 graphing application/MySwingApplication.java
+++ labs/lab 131 graphing application/MySwingApplication.java
@@ -12,11 +12,16 @@ public class MySwingApplication implements Runnable
     public void run(){
         JFrame jFrame = new JFrame("Window"); 
         jFrame.setSize(500,679);
+        CanvasComponent canvasComponent = new CanvasComponent(40, 2);
+        jFrame.add(canvasComponent); 
         jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
         jFrame.setVisible(true);
     }
 
     public void main(){
+        MySwingApplication mySwingApplication = new MySwingApplication(); 
+        javax.swing.SwingUtilities.invokeLater(mySwingApplication); 
+
     }
 
 }
