46c3c226cc6f15ce6055de224b6a9b54896c6812
795268 <42563195+795268@users.noreply.github.com>
2019-02-04 09:42:50 -0800

    started activity 2

---
+++ labs/lab 131 graphing application/CanvasComponent.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class CanvasComponent here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.awt.*; 
+import javax.swing.*;
+public class CanvasComponent extends JComponent
+{
+    
+}
