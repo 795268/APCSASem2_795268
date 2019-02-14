3d55553049486b16f3fc513e983c1430d2897321
795268 <42563195+795268@users.noreply.github.com>
2019-02-14 10:24:12 -0800

    Finished Activity 1

--- projects/Project 6 Elevens/Activity1 Starter Code/CardTester.java
+++ projects/Project 6 Elevens/Activity1 Starter Code/CardTester.java
@@ -9,5 +9,11 @@ public class CardTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card c1 = new Card ("Spades", "7", 7); 
+		Card c2 = new Card ( "Hearts", "7", 7); 
+		Card c3 = new Card ("Clubs" , "7", 7); 
+		
+		System.out.println (c3.toString()); 
+		System.out.println (c2.matches(c1)); 
 	}
 }
