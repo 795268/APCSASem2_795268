913dd9573eb78f9f1e433406d1fba72df0a0b74c
795268 <42563195+795268@users.noreply.github.com>
2019-02-25 10:27:30 -0800

    finished activity 2

--- projects/Project 6 Elevens/Activity2  Starter Code/DeckTester.java
+++ projects/Project 6 Elevens/Activity2  Starter Code/DeckTester.java
@@ -1,6 +1,7 @@
 /**
  * This is a class that tests the Deck class.
  */
+import java.util.ArrayList; 
 public class DeckTester {
 
 	/**
@@ -9,5 +10,23 @@ public class DeckTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		String[] rank1 = { "King", "Queen", "Jack"}; 
+		String[] suit1 = {"Spade", "Heart", "Diamond"};
+		int [] value1 = { 13, 12,11} ; 
+		Deck deck1 = new Deck ( rank1, suit1, value1); 
+		System.out.println (deck1.size()); 
+		
+		String[] rank2 = { "5", "6", "7"}; 
+		String[] suit2 = {"Spade", "Heart", "Diamond"};
+		int [] value2 = {  5,6,7} ; 
+		Deck deck2 = new Deck ( rank2, suit2, value2); 
+		System.out.println (deck2.size()); 
+		
+		String[] rank3 = { "7", "8", "9"}; 
+		String[] suit3 = {"Spade", "Heart", "Diamond"};
+		int [] value3 = {7,8,9} ; 
+		Deck deck3 = new Deck ( rank3, suit3, value3); 
+		System.out.println (deck3.size()); 
+		
 	}
 }
