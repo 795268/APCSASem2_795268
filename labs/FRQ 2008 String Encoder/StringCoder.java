f2e82f84718862c59d2e39a1ec38eafc9753ebde
795268 <42563195+795268@users.noreply.github.com>
2019-03-21 07:05:24 -0700

    ?

--- labs/FRQ 2008 String Encoder/StringCoder.java
+++ labs/FRQ 2008 String Encoder/StringCoder.java
@@ -2,10 +2,11 @@
 /**
  * Write a description of class StringCoder here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Elena Campell)
+ * @version (2008 FRQ string coder)
  */
 import java.util.*;
+
 public class StringCoder
 {
     private String masterString;
@@ -20,16 +21,23 @@ public class StringCoder
      * @return the string obtained by concatenating the parts of the master string
      */
     public String decodeString(ArrayList<StringPart> parts)
-    {
-        String word = ""; 
-        StringPart s = new StringPart(int start, int length); 
-        for (int i = 0; i < parts.size(); i ++){
-         int end = s.getStart(); 
-         int length = s.getLength(); 
-         String temp = 
-        }
+    { 
+        /* to be implemented in part (a) */ 
+        // Write the StringCoder method decodeString. This method retrieves the substrings in the master
+        // string represented by each of the StringPart objects in parts, concatenates them in the order in
+        // which they appear in parts, and returns the result.
+        // Complete method decodeString below.
+        // /** @param parts an ArrayList of string parts that are valid in the master string
+        // * Precondition: parts.size() > 0
+        // * @return the string obtained by concatenating the parts of the master string
+        String word = "";
+        for(int i = 0; i < parts.size(); i++){
+            int end = nextPart.getStart()+nextPart.getLength();
+            word += masterString.substring(nextPart.getStart(), end);
+        } 
+        return word;
+        
     }
-
     /** @param str the string to encode using the master string
      * Precondition: all of the characters in str appear in the master string;
      * str.length() > 0
@@ -46,21 +54,15 @@ public class StringCoder
      * to create word
      */
     public ArrayList<StringPart> encodeString(String word)
-    { 
-        ArrayList<StringPart> list = new ArrayList(); 
-        String str = word; 
-        while (str.length()>0){
-            StringPart s = findPart(str); 
-            list.add(5);
-            int length = s.getLength(); 
-            if (length <= str.length()){
-                str = str.substring(length); 
-            }
+    { /* to be implemented in part (b) */ 
+        ArrayList<StringPart> parts = new ArrayList<StringPart>();
+        while(word.length() >0 ){
+            StringPart nextPart = findPart(word);
+            parts.add(nextPart);
+            word = word.substring(nextPart.getLength());
         }
-        return list; 
-       }
-   // There may be instance variables, constructors, and methods that are not shown.
-}
-
- 
-
+        return parts;
+        
+        }
+    }
+    // There may be instance variables, constructors, and methods that are not shown.
