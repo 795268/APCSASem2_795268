cfca518b6f56d17425dc900435251694b783d8cf
795268 <42563195+795268@users.noreply.github.com>
2019-04-15 12:11:51 -0700

    array 415

---
+++ labs/Array review 415/someClass.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class someClass here.
+ *
+ * @author (Elena Campell)
+ * @version (415)
+ */
+import java.util.*; 
+public class someClass
+{
+   ArrayList<String> names = new ArrayList<String>(); 
+public void sortStrings(ArrayList<String> names){
+    //sortStrings sorts the contents on names into ascending order
+
+}
+public void shuffleStrings(ArrayList<String> names){
+//shuffleStrings() should shuffle the contents of names
+
+}
+public void insertString(String word){
+//this method will insert the parameter "word" into the sorted array
+// at the location where the arrayList remains sorted.
+
+}
+}
